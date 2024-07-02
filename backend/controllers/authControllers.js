11f9b7908b2b0c58d2e0b7c4596f9b8f384c4b03
Manikant Jha <manikantcodes@gmail.com>
2024-07-02 15:01:04 +0530

    auth-workflow

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 const { sendVerificationEmail } = require("../utils/emailUtils");
 const { sendErrorResponse } = require("../utils/serverUtils");
 const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   try {
@@ -54,21 +55,108 @@ const verifyEmail = async (req, res) => {
     }
 
     if (existingUser.isVerified) {
-      return sendErrorResponse(res, "Email is already verified..", 400);
+      return sendErrorResponse(res, "Email is already verified.", 400);
     }
+
+    existingUser.verificationToken = "";
+    existingUser.isVerified = true;
+    existingUser.verifiedAt = new Date();
+
+    await existingUser.save();
+
+    res
+      .status(200)
+      .json({ success: true, msg: "Email verified successfully." });
   } catch (error) {
     sendErrorResponse(res, error.message);
   }
 };
 
 const login = async (req, res) => {
-  res.send("login");
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return sendErrorResponse(res, "No such email exists.", 404);
+    }
+
+    if (!existingUser.isVerified) {
+      return sendErrorResponse(res, "Account not verified.", 401);
+    }
+
+    if (existingUser.password !== password) {
+      return sendErrorResponse(res, "Invalid password.", 401);
+    }
+
+    const tokenUser = {
+      userId: existingUser._id,
+      userName: existingUser.fname + " " + existingUser.lname,
+      role: existingUser.role,
+    };
+
+    const accessToken = jwt.sign(tokenUser, "secret", { expiresIn: "1d" });
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: false,
+      signed: true,
+      expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
+    });
+
+    res.status(200).json({ success: true, msg: "Logged in successfuly." });
+  } catch (error) {
+    sendErrorResponse(res, error.message);
+  }
 };
 
 const logout = async (req, res) => {
-  res.send("logout");
+  try {
+    const { accessToken } = req.signedCookies;
+
+    if (!accessToken) {
+      sendErrorResponse(res, "No token provided.", 401);
+    }
+
+    const tokenUser = jwt.verify(accessToken, "secret");
+
+    res.cookie("accessToken", "", {
+      httpOnly: true,
+      expires: new Date(Date.now()),
+    });
+
+    res.status(200).json({ success: true, msg: "Logged out successfully." });
+  } catch (error) {}
 };
 const forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    if (!email) {
+      return sendErrorResponse(res, "Email is required.", 400);
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return sendErrorResponse(res, "No such user exists.", 404);
+    }
+
+    const resetPasswordToken = crypto.randomBytes(40).toString("hex");
+
+    // await sendResetPasswordEmail({
+    //   email: user.email,
+    //   token: passwordToken,
+    // });
+    const tenMinutes = 1000 * 60 * 10;
+    const resetPasswordTokenExpiry = new Date(Date.now() + tenMinutes);
+
+    existingUser.resetPasswordToken = passwordToken;
+    existingUser.resetPasswordTokenExpiry = passwordTokenExpirationDate;
+
+    await existingUser.save();
+  } catch (error) {}
   res.send("forgotPassword");
 };
 const resetPassword = async (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const { start } = require("./utils/serverUtils");
 const authRouter = require("./routes/authRouter");
 
 const app = express();
 
 app.use(cors());
+app.use(cookieParser("secret"));
 app.use(express.json());
 
 app.use("/auth", authRouter);
